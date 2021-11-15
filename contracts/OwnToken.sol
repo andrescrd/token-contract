c6bf1d64e35b243d0269e3242d7ab44853037331
andrescrd <c.andresricardo@gmail.com>
2021-11-15 15:06:48 -0300

    fix

--- contracts/OwnToken.sol
+++ contracts/OwnToken.sol
@@ -6,11 +6,11 @@ contract Token is ERC20Interface {
     string public name = "Token";
     string public symbol = "TOK";
     uint public decimals = 8;
-    uint public override totalSupply;
+    uint public totalSupply;
 
     address public founder;
     mapping(address => uint) balances;
-    mapping(address => mapping(address => bool)) allowed;
+    mapping(address => mapping(address => uint)) allowed;
 
     constructor() {
         founder = msg.sender;
@@ -19,13 +19,13 @@ contract Token is ERC20Interface {
     }
 
     function balanceOf(address _owner) public override view returns (uint256 balance){
-        return balances[tokenOwner];
+        return balances[_owner];
     }
 
     function transfer(address _to, uint256 _value) public override returns (bool success){
         require(_value <= balances[msg.sender]);
 
-        balances[to] += _value;
+        balances[_to] += _value;
         balances[msg.sender] -= _value;
 
         emit Transfer(msg.sender, _to, _value);
@@ -33,7 +33,7 @@ contract Token is ERC20Interface {
     }
 
     function allowance(address _owner, address _spender) public override view returns (uint256 remaining){
-        return allowed[tokenOwner][spender];
+        return allowed[_owner][_spender];
     }
     
     function approve(address _spender, uint256 _value) public override returns (bool success){
@@ -48,12 +48,12 @@ contract Token is ERC20Interface {
 
     function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success){
         require(balances[_from] >= _value);
-        require(allowed[_from][to] >= _value);
+        require(allowed[_from][_to] >= _value);
         require(_value > 0);
 
         balances[_to] += _value;
         balances[_from] -= _value;
-        allowed[_from][to] -= _value;
+        allowed[_from][_to] -= _value;
 
         emit Transfer(_from, _to, _value);
         return true;
